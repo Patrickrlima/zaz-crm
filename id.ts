51d4d789c557583f192c75dbf82ae04e72099aa1
Patrick Lima <patricklimarr@gmail.com>
2026-08-01 02:36:50 -0300

    Correção da estrutura de pastas

--- id.ts
+++
@@ -1,5 +0,0 @@
-export function generateId(prefix = ''): string {
-  const random = Math.random().toString(36).slice(2, 10);
-  const time = Date.now().toString(36);
-  return `${prefix}${prefix ? '_' : ''}${time}${random}`;
-}
